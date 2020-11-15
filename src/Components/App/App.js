d82e6ec1a922eacb7235d51b1ee534d577e06f3a
carriepresley <carriepresley15@gmail.com>
2020-11-15 15:30:54 -0600

    initial

---
+++ src/Components/App/App.js
@@ -0,0 +1,27 @@
+import './App.css'
+import SearchBar from '../SearchBar/SearchBar.js';
+import SearchResults from '../SearchResults/SearchResults.js';
+import Playlist from '../Playlist/Playlist.js';
+import React from 'react';
+
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+    <h1>Ja<span className="highlight">mmm</span>ing</h1>
+    <div className="App">
+     <SearchBar/>
+      <div className="App-playlist">
+        <SearchResults/>
+        <Playlist/>
+      </div>
+    </div>
+  </div>
+    );
+
+  }
+}
+
+export default App;
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SearchResults.css";
+
+class SearchResults extends React.Component {
+    render(){
+        return (
+            <div className="SearchResults">
+            <h2>Results</h2>
+            {/*Tracklist Component*/}
+          </div>
+        );
+    }
+}
+    
+  
+  export default SearchResults;

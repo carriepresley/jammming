e3f293aa9928bb85d0b667f89ff0f6b3ac54508a
carriepresley <carriepresley15@gmail.com>
2020-11-17 16:24:28 -0600

    map repair

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -8,15 +8,16 @@ import Track from "../Track/Track";
 class TrackList extends React.Component {
     render(){
         return (
-            <div className="TrackList">
-            {this.props.tracks.map((track) =>{
-                    return <Track 
-                    key = {track.id}
-                    track = {track}
-                    />
-                })
-            }
-            </div>
+            
+              <div className="TrackList">
+        {this.props.tracks && this.props.tracks.map((track) =>{
+                return <Track 
+                key = {track.id}
+                track = {track}
+                />
+            })
+        }
+        </div>
         )
     }
 }
